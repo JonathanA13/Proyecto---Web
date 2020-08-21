b12dd5e2239febe77b7ba4c29cf3af2286be359d
Jonathan <jonathan.alarcon@epn.edu.ec>
2020-08-21 01:21:09 -0500

    Hasta DTO

---
+++ Vuelos-593/viajes/src/Reserva/dtoReserva/reserva.create-dto.ts
@@ -0,0 +1,17 @@
+import {IsAlpha, IsDate, IsNotEmpty, MaxLength, MinLength} from "class-validator";
+
+export class ReservaCreateDto {
+
+    @IsDate()
+    fechaReserva: Date;
+
+    @IsAlpha()
+    @MinLength(3)
+    @MaxLength(20)
+    @IsNotEmpty()
+    tipo: String;
+
+    @IsDate()
+    horaAbordaje: String;
+
+}
